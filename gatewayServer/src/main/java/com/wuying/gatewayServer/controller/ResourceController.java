9003b29d545f33b3ed8b37d26ea873b7f09439bd
Administrator <myg13411164699@zohomail.com>
2025-11-05 19:45:29 +0800

    all changed to suit local running

---
+++ gatewayServer/src/main/java/com/wuying/gatewayServer/controller/ResourceController.java
@@ -0,0 +1,33 @@
+package com.wuying.gatewayServer.controller;
+
+import com.wuying.common.util.Util;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class ResourceController {
+
+
+
+    @GetMapping("/getResource")
+    public Mono<ResponseEntity<Void>> getResource(
+            @RequestParam("protected_level") String protectedLevel,
+            @RequestParam("type") String type,
+            @RequestParam("format") String format,
+            @RequestParam("resource_name") String resourceName,
+            @RequestParam("file_extension") String fileExtension) {
+
+        String baseInternalUri = "/internal_protected";
+        String internalUri = baseInternalUri + Util.encodePath(fileExtension, protectedLevel, type, format, resourceName);
+
+        System.out.println("Generated internal URI:" + internalUri);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Accel-Redirect", internalUri);
+        return Mono.just(ResponseEntity.ok().headers(headers).build());
+    }
+}
