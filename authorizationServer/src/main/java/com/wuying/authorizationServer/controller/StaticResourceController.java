009495c4731030fc437efb6400f13caddd0521c4
Administrator <myg13411164699@zohomail.com>
2025-11-05 22:27:01 +0800

    all changed to suit local running

--- authorizationServer/src/main/java/com/wuying/authorizationServer/controller/StaticResourceController.java
+++ authorizationServer/src/main/java/com/wuying/authorizationServer/controller/StaticResourceController.java
@@ -1,4 +1,4 @@
-package com.wuying.gatewayServer.controller;
+package com.wuying.authorizationServer.controller;
 
 import com.wuying.common.util.Util;
 import org.springframework.http.HttpHeaders;
@@ -7,25 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @RestController
-public class ResourceController {
+public class StaticResourceController {
     @GetMapping("/{protected_level}/getResource")
-    public Mono<ResponseEntity<Void>> getResource(
+    public ResponseEntity<Void> getResource(
             @PathVariable("protected_level") String protectedLevel,
             @RequestParam("type") String type,
             @RequestParam("format") String format,
             @RequestParam("resource_name") String resourceName,
             @RequestParam("file_extension") String fileExtension) {
-
         String baseInternalUri = "/internal_protected";
-        String internalUri = baseInternalUri + Util.encodePath(format, protectedLevel, type, format, resourceName);
-        System.out.println("resourceName:" + resourceName);
-        System.out.println("Generated internal URI:" + internalUri);
-
+        String internalUri = baseInternalUri + Util.encodePath(fileExtension, protectedLevel, type, format, resourceName);
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Accel-Redirect", internalUri);
-        return Mono.just(ResponseEntity.ok().headers(headers).build());
+        System.out.println(internalUri);
+        System.out.println(resourceName);
+        return ResponseEntity.ok().headers(headers).build();
     }
 }
--- authorizationServer/src/main/java/com/wuying/authorizationServer/security/handler/exceptionHandler/CustomAuthenticationEntryPoint.java
+++ authorizationServer/src/main/java/com/wuying/authorizationServer/security/handler/exceptionHandler/CustomAuthenticationEntryPoint.java
@@ -17,7 +17,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         RequestCache requestCache = new HttpSessionRequestCache();
         requestCache.saveRequest(request, response);
 
-        String redirectUrl = "/public/getResource?type=static&format=html&resource_name=loginPage";
+        String redirectUrl = "/public/getResource?type=static&format=html&resource_name=loginPage&file_extension=html";
         response.sendRedirect(redirectUrl);
         response.setContentType("text/html;charset=utf-8");
 
