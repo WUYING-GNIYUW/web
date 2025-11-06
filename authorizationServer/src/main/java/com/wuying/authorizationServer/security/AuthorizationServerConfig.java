04a36adc3fda1907cc23cff6efb1660932379cf9
Administrator <myg13411164699@zohomail.com>
2025-11-06 15:58:07 +0800

    all changed to suit local running

---
+++ authorizationServer/src/main/java/com/wuying/authorizationServer/security/AuthorizationServerConfig.java
@@ -0,0 +1,32 @@
+package com.wuying.authorizationServer.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+
+@Configuration
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class AuthorizationServerConfig {
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(getCustomClientRegistration());
+    }
+
+    private ClientRegistration getCustomClientRegistration() {
+        return ClientRegistration.withRegistrationId("gateway-client-registration")
+                .clientId("gateway-client-id")
+                .clientSecret("000000")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("http://localhost/login/oauth2/code/gatewayClient")
+                .scope("openid")
+                .authorizationUri("http://authorizationserver:8100")
+                .clientName("gateway-client")
+                .build();
+    }
+}
