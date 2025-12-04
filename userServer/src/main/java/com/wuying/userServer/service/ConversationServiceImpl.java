8e042201207d5b862b03a76722e8ec2c4c83a781
Administrator <myg13411164699@zohomail.com>
2025-12-04 15:15:48 +0800

    all changed to suit ECS running

--- userServer/src/main/java/com/wuying/userServer/service/ConversationServiceImpl.java
+++ userServer/src/main/java/com/wuying/userServer/service/ConversationServiceImpl.java
@@ -3,6 +3,7 @@ package com.wuying.userServer.service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wuying.common.pojo.ChatMessage;
 import com.wuying.common.pojo.Result;
+import com.wuying.common.pojo.User;
 import com.wuying.userServer.mapper.ChatMessageMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ import org.springframework.stereotype.Service;
 
 import java.security.Principal;
 import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -37,6 +41,31 @@ public class ConversationServiceImpl extends ServiceImpl<ChatMessageMapper, Chat
         return Result.<Boolean>builder().data(StorageMessage(chatMessage)).build();
 
     }
+    @Override
+    public Result<List<ChatMessage>> readHistoryMessage(String queriedUserId, Principal principal) {
+        String customClientUserId = principal.getName();
+        List<ChatMessage> historyMessageListList = lambdaQuery()
+                .and(w -> w.eq(ChatMessage::getSendUserId, customClientUserId)
+                        .eq(ChatMessage::getReceivedUserId, queriedUserId))
+                .or(w -> w.eq(ChatMessage::getSendUserId, queriedUserId)
+                        .eq(ChatMessage::getReceivedUserId, customClientUserId))
+                .orderByDesc(ChatMessage::getCreatedTime) // 按时间降序
+                .list();
+//        List<ChatMessage> historyMessageListList = Stream
+//                .concat(
+//                        lambdaQuery()
+//                                .eq(queriedUserId != null, ChatMessage::getSendUserId, queriedUserId)
+//                                .eq(customClientUserId != null, ChatMessage::getReceivedUserId, customClientUserId)
+//                                .list().stream(),
+//                        lambdaQuery()
+//                                .eq(customClientUserId != null, ChatMessage::getSendUserId, customClientUserId)
+//                                .eq(queriedUserId != null, ChatMessage::getReceivedUserId, queriedUserId)
+//                                .list().stream()
+//                ).toList();
+     return Result.<List<ChatMessage>>builder().data(historyMessageListList).build();
+    }
+
+
 
     private Boolean StorageMessage(ChatMessage chatMessage) {
         return save(chatMessage);
